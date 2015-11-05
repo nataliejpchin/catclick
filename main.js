f74e72cc3550c324c5628681f2984646d3908919
Natalie Chin <natalie.j.chin@gmail.com>
2015-11-04 20:05:36 -0600

    version 1 of project - add a cat image and increment count on click

---
+++ main.js
@@ -0,0 +1,9 @@
+﻿var cat = document.getElementById('cat-img');
+var number = document.getElementById('number');
+var count = 0;
+
+cat.addEventListener('click', function(){
+  count++;
+  number.innerHTML = count;  
+  
+}, false);
